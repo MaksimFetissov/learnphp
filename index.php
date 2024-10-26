68afb7a0198a6b965c2c05ccdbe08f9942edfc96
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-26 11:41:21 +0300

    Boolean logic

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
 
-$text = 'hello' . 'world';
-$text .= 'asdasdasd' . 123;
-$text = str_repeat('asdasd', 3);
-$text = strtoupper('hello');
-$text = 'aa';
-$text++;
-$num = 1;
-$num++;
-$num--;
-
-var_dump($num);
+$bool = true && true;
+$bool = false || false;
+$bool = !true;
+$bool = (true xor false);
+//                              
+$bool = true && false || true && false && !(false || true);
+$bool = true && var_dump('hello');
+var_dump($bool);
