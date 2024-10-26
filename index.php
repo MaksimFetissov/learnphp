2aa58d95c79bf265f3d5bba557e96f044aad909a
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-26 13:04:06 +0300

    while loop

--- index.php
+++ index.php
@@ -1,24 +1,7 @@
 <?php
-
-for($i=0;$i<10;$i++){
-    var_dump($i);
+$time = microtime(true);
+$i = 0;
+while($time + 10 > microtime(true)){
+    $i++;
 }
-
-for($i=9;$i>=0;$i--){
-    var_dump($i);
-}
-
-for($i=1;$i<1000000;$i*=2){
-    var_dump($i);
-}
-
-for($i=1;$i>0;$i++){
-    if($i%2 == 0){
-        continue;
-    }
-    if($i>100){
-        break;
-    }
-    var_dump($i);
-
-}
+var_dump($i);
