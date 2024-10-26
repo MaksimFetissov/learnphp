3e058e05ada197ac318bfae87ab004b74bc3879a
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-26 13:40:34 +0300

    Arrays

--- index.php
+++ index.php
@@ -2,23 +2,27 @@
 
 $array = ['apple', 'pear', 'cherry'];
 
-foreach($array as $value) {
-    var_dump($value);
-}
+$answer = implode(';',$array);
 
 $array = [
-    'first' => 'apple',
-    'second' => 'pear',
-    'third' => 'cherry',
+    'hello' => 'world',
+    1,
+    2,
+    4,
+    'something' => 'else',
+    10 => 5,
+    6,
+    7,
 ];
-
-
-foreach($array as $key=>$value) {
-    var_dump($key, $value);
-}
-
-foreach($array as &$value) { // pointer v pass-by-reference
-    $value = 'something else';
-    var_dump($value);
-}
-var_dump($array);
+var_dump($array['something']);
+var_dump($array[2]);
+$array = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+];
+var_dump($array[1][2]);
+$array = [[[1]]];
+var_dump($array[0][0][0]);
+$array = [[[[1]]]];
+var_dump($array[0][0][0][0]);
