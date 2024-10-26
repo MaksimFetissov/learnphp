b0aa6c1326f5ee7dcf67bcb970c258906a16941a
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-26 10:37:12 +0300

    Add types

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-echo 'Hello PHP';
+$num = 1;
+$num = 2.3;
+$string = 'text';
+$bool = true;
+$bool = false;
+$null = null;
+$var;
+$array = ['cherry', 'apple', 'pear', 1, null, false, true];
+$array = array('cherry', 'apple', 'pear', 1, null, false, true);
+$object = new stdClass();
 
-?>
+var_dump($object);
