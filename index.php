eee2a6776459ce3e8d022991ed1894c67bf4ea15
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-26 12:36:32 +0300

    For loop

--- index.php
+++ index.php
@@ -1,52 +1,24 @@
 <?php
-$num = 1;
-if($num < 10){
-    echo 'Is smaller than 10';
-} else if($num > 10){
-    echo 'Is bigger than 10';
-} else {
-    echo 'Is equal than 10';
-}
-$date = '25.10.2024';
-$weekday = (int) date('w', strtotime($date));
-var_dump($weekday);
 
-if($weekday == 1) {
-    echo 'Esmaspäev';
-} else if($weekday == 2) {
-    echo 'Teisipäev';
-} else if($weekday == 3) {
-    echo 'Kolmapäev';
-} else if($weekday == 4) {
-    echo 'Neljapäev';
-} else if($weekday == 5 || $weekday == 6) {
-    echo 'Pidupäev';
-} else if($weekday == 0) {
-    echo 'Pühapäev';
-} else {
-    echo 'Imelikpäev';
+for($i=0;$i<10;$i++){
+    var_dump($i);
 }
 
-switch ($weekday) {
-    case 1:
-        echo 'Esmaspäev';
+for($i=9;$i>=0;$i--){
+    var_dump($i);
+}
+
+for($i=1;$i<1000000;$i*=2){
+    var_dump($i);
+}
+
+for($i=1;$i>0;$i++){
+    if($i%2 == 0){
+        continue;
+    }
+    if($i>100){
         break;
-    case 2:
-        echo 'Teisipäev';
-        break;
-    case 3:
-        echo 'Kolmapäev';
-        break;
-    case 4:
-        echo 'Neljapäev';
-        break;
-    case 5:
-    case 6:
-        echo 'Pidupäev';
-        break;
-    case 0:
-        echo 'Pühapäev';
-        break;
-    default: 
-        echo 'Imelikpäev';
+    }
+    var_dump($i);
+
 }
