23ec8136d5b5986f559a08e1a9ea76c441e3bc93
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-31 15:05:23 +0200

    Static vs Dynamic

--- index.php
+++ index.php
@@ -1,40 +1,23 @@
 <?php
-// code author
 
-class Job {
-    public function task(Logger $logger){
-        for($i=0;$i<10;$i++){
-            $logger->log("Task $i complete!");  
-        }
+class Box {
+    public static $count;
+    public $length;
+    public static function test(){
+        var_dump(self::$count);
+    }
+
+    public function test2(){
+        var_dump($this->length);
+        var_dump(self::$count);
     }
 }
 
-class ConsoleLogger implements Logger {
-    public function log($message){
-        echo $message . "\n";
-    }
-}
-
-interface Logger {
-    public function log($message);
-}
-
-
-// code user
-class FileLogger implements Logger {
-    public function log($message){
-        $file = fopen('./log.txt', 'a');
-        fwrite($file, $message. "\n");
-        fclose($file);
-    }
-}
-
-class NothingLogger implements Logger {
-    public function log($message){
-
-    }
-}
-
-$job = new Job();
-$logger = new NothingLogger();
-$job->task($logger);
+$box1 = new Box();
+// $box2 = new Box();
+Box::$count = 1;
+Box::$count = 2;
+Box::test();
+$box1->test2();
+// var_dump($box1, $box2);
+var_dump(Box::$count, Box::$count);
