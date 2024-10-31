928154af5bdf161ae672c7bc906e826374d5d292
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-10-31 14:03:33 +0200

    Classes, objects, traits, magic methods

--- index.php
+++ index.php
@@ -1,42 +1,113 @@
 <?php
 
-function hello(){
-    var_dump('hello');
+class Box {
+
+    use Colorful;
+
+    public $length;
+    private $width;
+    protected $height;
+
+    public function __construct($length, $width, $height) {
+        $this->length = $length;
+        $this->height = $height;
+        $this->width = $width;
+    }
+
+    public function volume(){
+        return $this->length * $this->width * $this->height;
+    }
+
+    public function getWidth(){
+        return $this->width;
+    }
+
+    public function setWidth($width){
+        if($width<0){
+            $this->width = 0;
+        } else {
+            $this->width = $width;
+        }
+    }
+
+    function __destruct() {
+        echo "Destroying " . static::class . "\n";
+    }
 }
 
-hello();
+class MetalBox extends Box {
+    public $weightPerUnit;
 
+    public function changeValue(){
+        $this->height = 10;
+    }
 
-function sum($a, $b){
-   return $a+$b;
+    public function totalWeight(){
+        return $this->weightPerUnit * $this->volume();
+    }
+
+    public function __toString()
+    {
+        return 'Im a metal box!';
+    }
+
+    public function __invoke()
+    {
+        echo 'Im a function now';
+    }
 }
 
-$answer = sum(3,5);
-$answer2 = sum(2,1);
-echo $answer . ' is answer and answer2 is ' . $answer2;
-// return will end function early
-// function isBiggerThanTen($a){
-//     if($a > 10){
-//         return true;
-//     }
-//     return false;
+trait Colorful {
+    private $color;
     
-// }
+    public function setColor($red, $green, $blue){
+        if($red<0 || $red>255){
+            $red = 0;
+        }
+        if($green<0 || $green>255){
+            $green = 0;
+        }
+        if($blue<0 || $blue>255){
+            $blue = 0;
+        }
+        $this->color = '#' . 
+            str_pad(dechex($red), 2, '0', STR_PAD_LEFT) .
+            str_pad(dechex($green), 2, '0', STR_PAD_LEFT) .
+            str_pad(dechex($blue), 2, '0', STR_PAD_LEFT);
+    }
 
-function isBiggerThanTen($a){
-    return $a > 10;
+    public function getColor(){
+        return $this->color;
+    }
 }
 
-var_dump(isBiggerThanTen(2));
-var_dump(isBiggerThanTen(19));
-
-
-function recursive($i){
-    var_dump($i);
-    if($i<10){
-        $i++;
-        recursive($i);
-    }   
+function makeBox(){
+    $box = new Box(1,2,3);
 }
 
-recursive(0);
+makeBox();
+
+
+$box1 = new Box(4, 5, 6);
+$box1->length = -10;
+$box1->setColor(255,0,0);
+
+var_dump($box1);
+var_dump($box1->volume());
+$box1 = 1;
+unset($box1);
+$box2 = new Box(7, 8, 9);
+
+var_dump($box2);
+var_dump($box2->volume());
+
+$metalBox1 = new MetalBox(1, 2, 3);
+$metalBox1->weightPerUnit = 3;
+$metalBox1->changeValue();
+var_dump($metalBox1);
+var_dump($metalBox1->volume());
+var_dump($metalBox1->totalWeight());
+
+echo $metalBox1;
+
+$metalBox1();
