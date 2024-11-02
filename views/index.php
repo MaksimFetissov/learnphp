dac7004559013d0cbbbb83fa1aed8e59d5786422
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-11-02 15:53:55 +0200

    Fix depending on server run location, implement Router

--- views/index.php
+++ views/index.php
@@ -1,16 +1,16 @@
-<?php include 'views/partials/header.php' ?>
+<?php include  __DIR__ . '/partials/header.php' ?>
 
-  <?php include 'views/partials/hero.php' ?>
-  <?php include 'views/partials/featured-posts.php' ?>
+  <?php include  __DIR__ . '/partials/hero.php' ?>
+  <?php include  __DIR__ . '/partials/featured-posts.php' ?>
 
   <div class="row g-5">
     <div class="col-md-8">
-      <?php include 'views/partials/posts.php' ?>
+      <?php include  __DIR__ . '/partials/posts.php' ?>
     </div>
 
     <div class="col-md-4">
-      <?php include 'views/partials/sidebar.php' ?>
+      <?php include  __DIR__ . '/partials/sidebar.php' ?>
     </div>
   </div>
 
-<?php include 'views/partials/pagefoot.php' ?>
+<?php include  __DIR__ . '/partials/pagefoot.php' ?>
