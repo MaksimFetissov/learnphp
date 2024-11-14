270eca5543571a64acff32a2cd808283b42a8f91
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-11-14 13:35:46 +0200

    Add image to post

--- views/partials/posts.php
+++ views/partials/posts.php
@@ -5,6 +5,9 @@
 
 <?php foreach($posts as $post): ?>
     <article class="blog-post">
+        <?php if($post->image): ?>
+            <img src="<?=$post->image?>" class="w-100">
+        <?php endif; ?>
         <h2 class="display-5 link-body-emphasis mb-1"><?=$post->title?></h2>
         <p><?=$post->body?></p>
     </article>
